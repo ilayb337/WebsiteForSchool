ce8dda13a18174d48310cacbbf6473f249ef98b3
ilayb337 <bibiochel@gmail.com>
2025-09-28 20:57:01 +0300

    full new change with the laptop files

--- WebsiteForSchool.py
+++ WebsiteForSchool.py
@@ -3,16 +3,16 @@ from nicegui import ui
 ui.add_head_html("<div dir=ltr>")
 
 with ui.row().classes("w-full justify-center gap-20"):
-    ui.label('welcome to UNDECIDED WEBSITE NAME!').style('color: #6E93D6; font-size: 200%')
+    ui.label('Sign up ').style('color: #6E93D6; font-size: 200%; font-family :  Open Sans')
 with ui.row().classes("w-full justify-center"):
     with ui.column():
         ui.input('full name')
-        ui.input('Email')
+        ui.input(placeholder="Email",validation={"need to contain @gmail.com": lambda value: "@gmail.com" in value})
         ui.input('Password')
     with ui.column():
         ui.label('Gender')
         ui.radio(['Male', 'Female', 'Other'], value='Male')
         with ui.row().classes("gap-700"):
-            ui.button('Sign up', on_click=lambda: ui.notify('Click')).classes('square w-600 h-600')
-            ui.link('to Login page', 'http://10.0.0.12:8080')
+            ui.button('Sign up', on_click=lambda: ui.notify('Click'))
+            ui.link('to Login page', 'LoginPage')
 ui.run()
